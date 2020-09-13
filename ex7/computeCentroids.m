d97e116cec8f1e82b99d7ffb4a6c3d35b821e50e
xiaoxin700 <gd.zhang@live.com>
2020-09-13 20:43:29 +0800

    ex7 graded

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -26,10 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
-
-
+  for i = 1:K
+      idx_i = find(idx==i);
+      centroids(i,:) = mean(X(idx_i,:));
+  end
 
 
 
--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -20,13 +20,13 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
-
-
+  for i = 1:size(X,1)
+      temp = zeros(K,1);
+      for j = 1:K
+          temp(j)=sqrt(sum((X(i,:)-centroids(j,:)).^2));
+      end
+      [~,idx(i)] = min(temp);
+  end
 % =============================================================
 
 end
